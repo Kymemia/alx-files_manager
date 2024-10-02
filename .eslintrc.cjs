ea7f78b05e78c9b26e9c1278b83693aaff9ddab9
Kymemia <kimemiachristopherr@gmail.com>
2024-10-02 16:03:12 +0300

    create a new user

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,37 @@
 module.exports = {
-  env: {
-    browser: true,         
-    es2021: true,          
-    node: true,            
-  },
-  extends: [
-    'eslint:recommended',  
-    'plugin:node/recommended',  
-  ],
-  parserOptions: {
-    ecmaVersion: 12,       
-    sourceType: 'module',  
-  },
-  rules: {
-    'no-unused-vars': 'warn',  
-    'no-console': 'off',       
-    'node/no-unsupported-features/es-syntax': [
-      'error',
-      { ignores: ['modules'] },
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
     ],
-  },
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+      'max-classes-per-file': 'off',
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
 };
-
---
+++ controllers/UsersController.js
@@ -0,0 +1,34 @@
+import crypto from 'crypto';
+import dbClient from '../utils/db.js';
+
+class UsersController {
+  static async postNew (req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exists' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    const newUser = {
+      email,
+      password: hashedPassword
+    };
+
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+}
+
+export default UsersController;
